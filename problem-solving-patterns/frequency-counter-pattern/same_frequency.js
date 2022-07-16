2caa6eb19ab7a39cf20eb8b48d382a1177da0171
Jeong hwajeong <azamwara@gmail.com>
2022-07-16 22:55:27 +0900

    Fix wrong solution

--- problem-solving-patterns/frequency-counter-pattern/same_frequency.js
+++ problem-solving-patterns/frequency-counter-pattern/same_frequency.js
@@ -16,13 +16,16 @@ function sameFrequency(num1, num2) {
   for (let i = 0; i < numArr2.length; i++) {
     if (!obj1[numArr2[i]]) {
       return false;
+    } else {
+      obj1[numArr2[i]]--;
     }
   }
 
   return true;
 }
 
-console.log(sameFrequency(182, 281));
-console.log(sameFrequency(34, 14));
-console.log(sameFrequency(3589578, 5879385));
-console.log(sameFrequency(22, 222));
+console.log(sameFrequency(182, 281)); //true
+console.log(sameFrequency(34, 14)); //false
+console.log(sameFrequency(3589578, 5879385)); //true
+console.log(sameFrequency(22, 222)); //false
+console.log(sameFrequency(284, 444)); //false
